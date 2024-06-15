39afdce0cc23ea06a990c58e6cc696626fdcc819
Ahmed Saad <ahmedsaad22502145@gmail.com>
2024-06-15 13:34:00 +0300

    adding decentralized network

---
+++ run_node2.py
@@ -0,0 +1,5 @@
+from app_nodex import app
+
+
+if __name__ == '__main__':  
+   app.run(port=5002, debug=False)
---
+++ run_node3.py
@@ -0,0 +1,5 @@
+from app_nodex import app
+
+
+if __name__ == '__main__':  
+   app.run(port=5003, debug=False)
